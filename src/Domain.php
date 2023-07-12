7bff53701dc8d734294a36d9d25e488f5a90aed6
Jayan Ratna <jayanratna@gmail.com>
2023-07-12 12:31:47 +1200

    fix: phpdocs for all Resources are outdated (#31)

--- src/Domain.php
+++ src/Domain.php
@@ -2,6 +2,12 @@
 
 namespace Resend;
 
+/**
+ * @property string $id The unique identifier for the domain.
+ * @property string $name The domain name.
+ * @property string $created_at Time at which the domain was created.
+ * @property array $records The list of DNS records to add to your domain.
+ */
 class Domain extends Resource
 {
     //
--- src/Email.php
+++ src/Email.php
@@ -3,10 +3,18 @@
 namespace Resend;
 
 /**
- * @property string $id The unique identifier for the API key.
+ * @property string $object The type of object.
+ * @property string $id The unique identifier for the email.
  * @property string $from The sender's email address.
- * @property string $to The email recipient address.
- * @property string $created_at Time at which the API key was created.
+ * @property array $to The email addresses of all recipients.
+ * @property string $created_at Time at which the email was created.
+ * @property string $subject The email subject.
+ * @property null|string $html The HTML representation of the email.
+ * @property null|string $text The text representation of the email.
+ * @property null|array $bcc The email addresses of all blind carbon copy recipients.
+ * @property null|array $cc The email addresses of all carbon copy recipients.
+ * @property null|array $reply_to The reply to email address.
+ * @property string $last_event The last event for the email.
  */
 class Email extends Resource
 {
