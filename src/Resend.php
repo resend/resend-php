59a96844f5a0878ceb9ebbeb7fddb0829d59d7d4
Jayan Ratna <jayanratna@gmail.com>
2023-09-16 09:28:40 +1200

    chore: bump to 0.8.0 (#41)

--- src/Resend.php
+++ src/Resend.php
@@ -12,7 +12,7 @@ class Resend
     /**
      * The current SDK version.
      */
-    public const VERSION = '0.7.2';
+    public const VERSION = '0.8.0';
 
     /**
      * Creates a new Resend Client with the given API key.
