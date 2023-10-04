36b4a1837621573c983c7123451ba6db79ebcc54
Jayan Ratna <jayanratna@gmail.com>
2023-10-04 17:29:00 +1300

    include required fields in test

--- tests/Service/Batch.php
+++ tests/Service/Batch.php
@@ -3,23 +3,24 @@
 use Resend\Collection;
 
 it('can send a  batch of emails', function () {
-    $client = mockClient('POST', 'emails/batch', [
+    $payload = [
         [
             'to' => 'test@resend.com',
+            'from' => 'noreply@resend.com',
+            'subject' => 'Acme',
+            'text' => 'it works!',
         ],
         [
             'to' => 'test@resend.com',
+            'from' => 'noreply@resend.com',
+            'subject' => 'Acme',
+            'text' => 'it works!',
         ],
-    ], batch());
+    ];
 
-    $result = $client->batch->send([
-        [
-            'to' => 'test@resend.com',
-        ],
-        [
-            'to' => 'test@resend.com',
-        ],
-    ]);
+    $client = mockClient('POST', 'emails/batch', $payload, batch());
+
+    $result = $client->batch->send($payload);
 
     expect($result)->toBeInstanceOf(Collection::class)
         ->data->toBeArray();
