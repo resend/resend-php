0a96858eb6b7349e38d07c5b599f096a28315f35
Jayan Ratna <jayanratna@gmail.com>
2023-10-04 15:51:58 +1300

    add batch service

--- src/Service/ServiceFactory.php
+++ src/Service/ServiceFactory.php
@@ -13,6 +13,7 @@ class ServiceFactory
      */
     private static array $classMap = [
         'apiKeys' => ApiKey::class,
+        'batch' => Batch::class,
         'domains' => Domain::class,
         'emails' => Email::class,
     ];
