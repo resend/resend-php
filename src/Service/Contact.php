7c8ea65d6a4d48fd1d4f30b9e3caa4f8f9f499c4
Jayan Ratna <jayanratna@gmail.com>
2023-12-19 22:06:02 +1300

    add get method to service

--- src/Service/Contact.php
+++ src/Service/Contact.php
@@ -7,7 +7,25 @@ use Resend\ValueObjects\Transporter\Payload;
 class Contact extends Service
 {
     /**
+     * Retrieve a single contact from an audience.
+     *
+     * @see https://resend.com/docs/api-reference/contacts/get-contact
+     */
+    public function get(string $audienceId, string $contactId): \Resend\Contact
+    {
+        $payload = Payload::get("audiences/$audienceId/contacts", $contactId);
+
+        $result = $this->transporter->request($payload);
+
+        return $this->createResource('contacts', $result);
+    }
+
+    /**
      * List all contacts from an audience.
+     *
+     * @return \Resend\Collection<\Resend\Contact>
+     *
+     * @see https://resend.com/docs/api-reference/contacts/list-contacts
      */
     public function list(string $audienceId): \Resend\Collection
     {
