697bac9552f653072ecb4113e982ab15e51e9e01
Jayan Ratna <jayanratna@gmail.com>
2023-12-20 18:46:17 +1300

    add tests for contacts

--- src/Client.php
+++ src/Client.php
@@ -11,7 +11,7 @@ use Resend\Service\ServiceFactory;
  *
  * @property \Resend\Service\ApiKey $apiKeys
  * @property \Resend\Service\Batch $batch
- * @property \Resend\Service\Contact $contact
+ * @property \Resend\Service\Contact $contacts
  * @property \Resend\Service\Domain $domains
  * @property \Resend\Service\Email $emails
  */
