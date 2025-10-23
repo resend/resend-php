7696d153e460a566247342fc48c41062daf5a44e
Jayan Ratna <jayanratna@gmail.com>
2025-10-24 10:40:54 +1300

    feat: add webhook service

---
+++ tests/Service/Webhook.php
@@ -0,0 +1,46 @@
+<?php
+
+use Resend\Collection;
+use Resend\Webhook;
+
+it('can get a webhook resource', function () {
+    $client = mockClient('GET', 'webhooks/4dd369bc-aa82-4ff3-97de-514ae3000ee0', [], [], webhook());
+
+    $result = $client->webhooks->get('4dd369bc-aa82-4ff3-97de-514ae3000ee0');
+
+    expect($result)->toBeInstanceOf(Webhook::class)
+        ->id->toBe('4dd369bc-aa82-4ff3-97de-514ae3000ee0');
+});
+
+it('can create a webhook resource', function () {
+    $client = mockClient('POST', 'webhooks', [
+        'endpoint' => 'https://webhook.example.com/handler',
+        'events' => ['email.sent', 'email.delivered', 'email.bounced'],
+    ], [], webhook());
+
+    $result = $client->webhooks->create([
+        'endpoint' => 'https://webhook.example.com/handler',
+        'events' => ['email.sent', 'email.delivered', 'email.bounced'],
+    ]);
+
+    expect($result)->toBeInstanceOf(Webhook::class)
+        ->id->toBe('4dd369bc-aa82-4ff3-97de-514ae3000ee0');
+});
+
+it('can get a list of webhook resources', function () {
+    $client = mockClient('GET', 'webhooks', [], [], webhooks());
+
+    $result = $client->webhooks->list();
+
+    expect($result)->toBeInstanceOf(Collection::class)
+        ->data->toBeArray();
+});
+
+it('can remove a webhook resource', function () {
+    $client = mockClient('DELETE', 'webhooks/4dd369bc-aa82-4ff3-97de-514ae3000ee0', [], [], webhook());
+
+    $result = $client->webhooks->remove('4dd369bc-aa82-4ff3-97de-514ae3000ee0');
+
+    expect($result)->toBeInstanceOf(Webhook::class)
+        ->id->toBe('4dd369bc-aa82-4ff3-97de-514ae3000ee0');
+});
