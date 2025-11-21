2da1bd917e36a4bd9a5b80135400c35c8880c4d6
Jayan Ratna <jayanratna@gmail.com>
2025-11-21 17:21:48 +1300

    bump version

--- src/Resend.php
+++ src/Resend.php
@@ -12,7 +12,7 @@ class Resend
     /**
      * The current SDK version.
      */
-    public const VERSION = '1.0.1';
+    public const VERSION = '1.1.0';
 
     /**
      * Creates a new Resend Client with the given API key.
