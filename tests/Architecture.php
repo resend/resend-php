eadc8544cff41eb1f04235f1c384b25f42e25556
Jayan Ratna <jayanratna@gmail.com>
2026-02-17 18:23:23 +1300

    add pest version check

--- tests/Architecture.php
+++ tests/Architecture.php
@@ -1,5 +1,8 @@
 <?php
 
-test('No debugging statements are left in the code')
-    ->expect(['var_dump'])
-    ->not->toBeUsed();
+// Only run on PEST 2.0 or higher...
+if (version_compare(Pest\version(), '2.0.0', '>=')) {
+    test('No debugging statements are left in the code')
+        ->expect(['var_dump'])
+        ->not->toBeUsed();
+}
