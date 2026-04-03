5759976943cd89a1f4cd5671d0bcddd16450e957
Jayan Ratna <jayanratna@gmail.com>
2026-04-03 14:40:13 +1300

    add resource types

--- src/Automations/Run.php
+++ src/Automations/Run.php
@@ -4,6 +4,15 @@ namespace Resend\Automations;
 
 use Resend\Resource;
 
+/**
+ * @property string $id The unique identifier for the automation run.
+ * @property string $object The type of object.
+ * @property string $status The run status.
+ * @property array $trigger The trigger details.
+ * @property string $started_at Time at which the run started.
+ * @property string $completed_at Time at which the run completed.
+ * @property string $created_at Time at which the run was created.
+ */
 class Run extends Resource
 {
     //
