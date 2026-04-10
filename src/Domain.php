30d6154b45185db741a8379d1f9997a9a6cba8cc
Jayan Ratna <jayanratna@gmail.com>
2026-04-10 18:00:08 +1200

    feat: add support for tracking domains

--- src/Domain.php
+++ src/Domain.php
@@ -3,11 +3,16 @@
 namespace Resend;
 
 /**
+ * @property string $object The object type identifier.
  * @property string $id The unique identifier for the domain.
  * @property string $name The domain name.
  * @property string $status The verification status of the domain.
  * @property string $created_at Time at which the domain was created.
  * @property string $region The region the domain is located in.
+ * @property bool $open_tracking Whether open tracking is enabled for the domain.
+ * @property bool $click_tracking Whether click tracking is enabled for the domain.
+ * @property string $tracking_subdomain The subdomain used for tracking links.
+ * @property array $capabilities The capabilities of the domain (sending, receiving, etc).
  * @property array $records The list of DNS records to add to your domain.
  */
 class Domain extends Resource
--- tests/Fixtures/Domain.php
+++ tests/Fixtures/Domain.php
@@ -4,11 +4,53 @@ function domain(): array
 {
     return [
         'object' => 'domain',
-        'id' => '4dd369bc-aa82-4ff3-97de-514ae3000ee0',
-        'name' => 'resend.dev',
-        'status' => 'pending',
+        'id' => 'd91cd9bd-1176-453e-8fc1-35364d380206',
+        'name' => 'example.com',
+        'status' => 'not_started',
+        'created_at' => '2026-04-26T20:21:26.347412+00:00',
         'region' => 'us-east-1',
-        'created_at' => '2022-07-25T00:28:32.493138+00:00',
+        'open_tracking' => true,
+        'click_tracking' => false,
+        'tracking_subdomain' => 'links',
+        'capabilities' => [
+            'sending' => 'enabled',
+            'receiving' => 'disabled',
+        ],
+        'records' => [
+            [
+                'record' => 'SPF',
+                'name' => 'send',
+                'type' => 'MX',
+                'ttl' => 'Auto',
+                'status' => 'not_started',
+                'value' => 'feedback-smtp.us-east-1.amazonses.com',
+                'priority' => 10,
+            ],
+            [
+                'record' => 'SPF',
+                'name' => 'send',
+                'value' => '"v=spf1 include:amazonses.com ~all"',
+                'type' => 'TXT',
+                'ttl' => 'Auto',
+                'status' => 'not_started',
+            ],
+            [
+                'record' => 'DKIM',
+                'name' => 'resend._domainkey',
+                'value' => 'p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDsc4Lh8xilsngyKEgN2S84+21gn+x6SEXtjWvPiAAmnmggr5FWG42WnqczpzQ/mNblqHz4CDwUum6LtY6SdoOlDmrhvp5khA3cd661W9FlK3yp7+jVACQElS7d9O6jv8VsBbVg4COess3gyLE5RyxqF1vYsrEXqyM8TBz1n5AGkQIDAQA2',
+                'type' => 'TXT',
+                'status' => 'not_started',
+                'ttl' => 'Auto',
+            ],
+            [
+                'record' => 'Tracking',
+                'name' => 'links.example.com',
+                'type' => 'CNAME',
+                'value' => 'links1.resend-dns.com',
+                'ttl' => 'Auto',
+                'status' => 'not_started',
+            ],
+        ],
     ];
 }
 
@@ -17,18 +59,15 @@ function domains(): array
     return [
         'data' => [
             [
-                'id' => '71af5cc3-b449-4ac4-888a-5ab9f55e1dbb',
-                'name' => 'resend.dev',
-                'status' => 'pending',
+                'id' => 'd91cd9bd-1176-453e-8fc1-35364d380206',
+                'name' => 'example.com',
+                'status' => 'not_started',
+                'created_at' => '2026-04-26T20:21:26.347412+00:00',
                 'region' => 'us-east-1',
-                'created_at' => '2022-07-25T00:28:32.493138+00:00',
-            ],
-            [
-                'id' => '823ad493-7081-4344-b476-fb0db4bd1e62',
-                'name' => 'resend.com',
-                'status' => 'pending',
-                'region' => 'us-east-1',
-                'created_at' => '2022-07-25T00:28:32.493138+00:00',
+                'capabilities' => [
+                    'sending' => 'enabled',
+                    'receiving' => 'disabled',
+                ],
             ],
         ],
     ];
