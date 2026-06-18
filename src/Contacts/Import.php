0536ce06898eb6d103f76e2a7047b6f03353d962
Jayan Ratna <jayanratna@gmail.com>
2026-06-18 18:21:12 +1200

    add import properties

--- src/Contacts/Import.php
+++ src/Contacts/Import.php
@@ -6,6 +6,10 @@ use Resend\Resource;
 
 /**
  * @property string $id The unique identifier for the contact import.
+ * @property string $status The contact import status.
+ * @property string $created_at The time at which the contact import was created.
+ * @property string $completed_at The time at which the contact import was completed.
+ * @property array $counts The imported contact counts.
  */
 class Import extends Resource
 {
