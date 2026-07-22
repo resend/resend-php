c28ffa2cce509b32b897b143d63c369dadd43274
Jayan Ratna <jayanratna@gmail.com>
2026-07-22 17:47:11 +1200

    add types to resource

--- src/Suppression.php
+++ src/Suppression.php
@@ -4,6 +4,11 @@ namespace Resend;
 
 /**
  * @property string $id The unique identifier for the suppression.
+ * @property string $object The type of object.
+ * @property string $email The email address.
+ * @property string $origin The origin of the suppression.
+ * @property string $source_id References the email that triggered the suppression. For suppressions with a manual origin, source_id is null.
+ * @property string $created_at Time at which the suppression was created.
  */
 class Suppression extends Resource
 {
