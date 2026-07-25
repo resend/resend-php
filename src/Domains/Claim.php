8a2528bd571c54bf82e7f7950016538427656299
Jayan Ratna <jayanratna@gmail.com>
2026-07-25 16:23:17 +1200

    fix types

--- src/Domains/Claim.php
+++ src/Domains/Claim.php
@@ -11,11 +11,7 @@ use Resend\Resource;
  * @property string $status The status of the domain claim.
  * @property string $domain_id The unique identifier of the placeholder domain created on your team.
  * @property string $region The region where emails will be sent from.
- * @property object $record The DNS record to add for verification.
- * @property string $record.type The DNS record type.
- * @property string $record.name The DNS record name.
- * @property string $record.value The DNS record value to add.
- * @property string $record.ttl The DNS record TTL.
+ * @property array $record The DNS record to add for verification.
  * @property string|null $blocked_reason The reason the domain claim was blocked, if applicable.
  * @property string|null $failure_reason The reason the domain claim failed, if applicable.
  * @property string $created_at The timestamp of when the domain claim was created.
